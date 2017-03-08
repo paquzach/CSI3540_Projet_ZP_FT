ff07e980c5c212f5e9d4e150fb1525183cc39c54
FrancisTP <francis-tremblay@hotmail.ca>
2017-03-08 15:45:31 -0500

    Added temporary background for game menu, buttons are completely fixed. Still need to implmement tiitle on title screen, game screen and highscore screen.

--- Server/public/javascript/game.js
+++ Server/public/javascript/game.js
@@ -7,17 +7,6 @@
 
  PIXI.utils.sayHello(type)
 
-
-/*
-.slideshow-container
-{
-	border: 3px solid #ffdd00; 
-	border-radius: 5px;
-	position: relative;
-	margin: auto;
-	margin-top: 5px;
-}
-*/
  var renderer = PIXI.autoDetectRenderer(800, 550);
  renderer.view.style.border = "3px solid #ffdd00";
  renderer.backgroundColor = 0xffffff;
@@ -32,6 +21,7 @@
 
  var currentScreen = "mainMenu";
 
+ var fruity_background;
  var play_button;
  var highscore_button;
 
@@ -43,22 +33,34 @@
 
  function setup() {
 
-	PIXI.loader.add('../gameTextures', '../gameTextures/ui.json').load(function(loader, resources) {
+	PIXI.loader.add('../gameTextures', '../gameTextures/ui.json').add('../gameTextures/fruity_background.json').load(function(loader, resources) {
 		loadMainMenu();
 	});
-
-
-	// render the stage
-	//renderer.render(stage);
  }
 
 function loadMainMenu() {
 	// PLAY BUTTON
+	fruity_background = new Background(400, 275, 800, 550,new PIXI.Sprite.fromFrame('fruity_background.jpg'));
+ 	play_button = new Button(280, 380, 190, 49, new PIXI.Sprite.fromFrame('green_button04.png'), new PIXI.Sprite.fromFrame('green_button03.png'), "PLAY");
+  	highscore_button = new Button(520, 380, 190, 49, new PIXI.Sprite.fromFrame('green_button04.png'), new PIXI.Sprite.fromFrame('green_button03.png'), "HIGHSCORE");
 
- 	play_button = new Button(210, 275, 190, 49, new PIXI.Sprite.fromFrame('green_button04.png'), new PIXI.Sprite.fromFrame('blue_button04.png'), "PLAY");
-  	highscore_button = new Button(590, 275, 190, 49, new PIXI.Sprite.fromFrame('green_button04.png'), new PIXI.Sprite.fromFrame('blue_button03.png'), "HIGHSCORE");
+  	play_button.container.mousedown = function(mousedata) {
+  		console.log("I WANT TO PLAY");
+  		loadGame();
+  	}
+  	highscore_button.container.mousedown = function(mousedata) {
+  		console.log("I WANT HIGHSCORES");
+  	}
 
  	requestAnimationFrame(animate);
+}
+
+function loadGame() {
+
+}
+
+function loadHighscores() {
+
 }
 
 
@@ -68,6 +70,7 @@ function animate() {
 	stage.removeChildren();
 	//stage.update();
 	if (currentScreen == "mainMenu") {
+		fruity_background.render();
 		play_button.render();
 		highscore_button.render();
 	}
@@ -84,6 +87,14 @@ function Button(x, y, width, height, light, dark, text) {
 	this.y = y;
 	this.width = width;
 	this.height = height;
+
+	this.text = new PIXI.Text(text, {font:"50px Arial", fontWeight: "bold", fill:"#000000", align:"center"});
+	this.text.x = this.x - (this.text.width / 2);
+	this.text.y = this.y - (this.text.height / 2);
+
+	if (this.text.width > (this.width - 16)) {
+		this.width = this.text.width + 16;
+	}
 	
 	this.light = light;
 	this.dark = dark;
@@ -97,10 +108,6 @@ function Button(x, y, width, height, light, dark, text) {
 	this.light.y = this.y - (this.light.height / 2);
 	this.dark.x = this.x - (this.dark.width / 2);
 	this.dark.y = this.y - (this.dark.height / 2);
-
-	this.text = new PIXI.Text(text, {font:"50px Arial", fill:"black", align:"center"});
-	this.text.x = this.x - (this.text.width / 2);
-	this.text.y = this.y - (this.text.height / 2);
 	
 	this.mouseOver = false;
 
@@ -119,11 +126,14 @@ function Button(x, y, width, height, light, dark, text) {
 	// methods
 	this.render = function() {
 
-		console.l
 		if (this.mouseOver) {
 			stage.addChild(this.dark);
+			this.text.style = {font:"50px Arial", fontWeight: "bold", fill:"#262626", align:"center"};
+			this.text.y = this.y - (this.text.height / 2) + 2;
 		} else {
 			stage.addChild(this.light);
+			this.text.style = {font:"50px Arial", fontWeight: "bold", fill:"#000000", align:"center"};
+			this.text.y = this.y - (this.text.height / 2);
 		}
 		stage.addChild(this.text);
 		stage.addChild(this.container);
@@ -134,5 +144,24 @@ function Button(x, y, width, height, light, dark, text) {
 	}
 	this.container.mouseout = function(mouseData){
 		this.p.mouseOver = false;
+	}
+}
+
+// Background
+function Background(x, y, width, height, background) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+
+	this.background = background;
+
+	this.background.x = this.x - (this.width / 2);
+	this.background.y = this.y - (this.height / 2);
+	this.background.width = this.width;
+	this.background.height = this.height;
+
+	this.render = function() {
+		stage.addChild(this.background);
 	}
 }
