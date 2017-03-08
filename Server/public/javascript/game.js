761b5c9cadcfa83242620eef17f8157549a8b66a
FrancisTP <francis-tremblay@hotmail.ca>
2017-03-07 19:32:41 -0500

    Working state

--- Server/public/javascript/game.js
+++ Server/public/javascript/game.js
@@ -28,9 +28,13 @@
  renderer.view.style.margin = "15px auto";
 
  var stage = new PIXI.Container();
- var background;
- var play_button_01;
- var play_button_02;
+ stage.interactive = true;
+
+ var currentScreen = "mainMenu";
+
+ var play_button;
+ var highscore_button;
+ var grphcs;
 
  console.log(type);
 
@@ -40,35 +44,92 @@
 
  function setup() {
 
-	PIXI.loader.add('../gameTextures/', 'ui.json').load(function(loader, resources) {
-		initialiseScene();
+	PIXI.loader.add('../gameTextures', '../gameTextures/ui.json').load(function(loader, resources) {
+		loadMainMenu();
 	});
-
 
 
 	// render the stage
 	renderer.render(stage);
  }
 
-function initialiseScene() {
-  createObjects();
-  //animate();
+function loadMainMenu() {
+	// PLAY BUTTON
+
+ 	play_button = new Button(210, 275, 190, 49, new PIXI.Sprite.fromFrame('green_button04.png'), new PIXI.Sprite.fromFrame('blue_button04.png'), "PLAY");
+  	highscore_button = new Button(590, 275, 190, 49, new PIXI.Sprite.fromFrame('green_button04.png'), new PIXI.Sprite.fromFrame('green_button03.png'), "HIGHSCORE");
+
+ 	requestAnimationFrame(animate);
 }
 
-function createObjects() {
-  //background = new PIXI.Sprite.fromFrame('bg.png');
-  play_button_01 = new PIXI.Sprite.fromFrame('blue_button04.png');
-  play_button_01.x = 400;
-  play_button_01.y = 275;
 
-  console.log("");
-  console.log("================================================");
-  console.log("play_button_01: " + play_button_01);
-  console.log("play_button_01.x: " + play_button_01.x);
-  console.log("play_button_01.y: " + play_button_01.y);
-  console.log("================================================");
-  console.log("");
 
-  play_button_02 = new PIXI.Sprite.fromFrame('blue_button03.png')
-  stage.addChild(play_button_01); 
+// UPDATE FUNCTIONS
+function animate() {
+	stage.removeChildren();
+	//stage.update();
+	if (currentScreen == "mainMenu") {
+		play_button.render();
+		highscore_button.render();
+		//stage.addChild(grphcs);
+	}
+
+	renderer.render(stage);
+    requestAnimationFrame(animate);
+}
+
+
+// OBJECTS
+// Button
+function Button(x, y, width, height, light, dark, text) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	
+	this.light = light;
+	this.dark = dark;
+
+	this.light.width = this.width;
+	this.light.height = this.height;
+	this.dark.width = this.width;
+	this.dark.height = this.height;
+
+	this.light.x = this.x - (this.width / 2);
+	this.light.y = this.y - (this.height / 2);
+	this.dark.x = this.x - (this.width / 2);
+	this.dark.y = this.y - (this.height / 2);
+	
+	this.light.alpha = 1;
+	this.dark.alpha = 0;
+
+	this.text = new PIXI.Text(text, {font:"50px Arial", fill:"black", align:"center"});
+	this.text.x = this.x - (this.text.width / 2);
+	this.text.y = this.y - (this.text.height / 2);
+	this.text.alpha = 1;
+	
+	this.mouseOver = false;
+
+	// methods
+	this.render = function() {
+		if (this.mouseOver) {
+			this.light.alpha = 0;
+			this.dark.alpha = 1;
+		} else {
+			this.light.alpha = 1;
+			this.dark.alpha = 0;
+		}
+		stage.addChild(this.light);
+		stage.addChild(this.dark);
+		stage.addChild(this.text);
+
+		console.log("mouseOver: " + this.mouseOver);
+	}
+
+	this.mouseover = function(mouseData){
+		this.mouseOver = true;
+	}
+	this.mouseOut = function(mouseData){
+		this.mouseOver = false;
+	}
 }
